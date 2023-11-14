b1be4f956361e1df0fd3b08348515e5cf1783792
RickyColotl <144842683+RickyColotl@users.noreply.github.com>
2023-11-13 23:22:59 -0500

    Added Client Survey Endpoint test

--- fitness-express-server/server.js
+++ fitness-express-server/server.js
@@ -22,6 +22,13 @@ app.post('/coach-survey', (req, res) => {
     res.status(201).json({ message: 'Coach Survey data recieved successfully!'});
   });
   
+  app.post('/client-survey', (req, res) => {
+    // Log the request body to see what's being sent by the client
+    console.log(req.body);
+  
+    // Send back a dummy response
+    res.status(201).json({ message: 'Client Survey data recieved successfully!'});
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

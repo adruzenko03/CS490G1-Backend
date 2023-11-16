84f1fb9e8e9e453dd2681659205ef22fa18e6d55
Alex <adruzenko03@gmail.com>
2023-11-16 14:17:45 -0500

    Login/Signup Logic

--- database.js
+++ database.js
@@ -1,11 +1,36 @@
 import mysql from 'mysql';
+import "dotenv/config.js";
 export default class DatabaseService {
     constructor(){
-        mysql.createConnection({
+        this.connection=mysql.createConnection({
             host     : 'localhost',
-            user     : 'me',
-            password : 'secret',
-            database : 'my_db'
+            user     : 'root',
+            password : process.env.DB_PASS,
+            database : 'fitness'
           })
+          this.connection.connect();
+          this.connection.query('SELECT 1 + 1 AS solution', (err,res)=>{
+            console.log("Successful Database Connection")
+          })
+    }
+    login(username,password,callback){
+        this.connection.query(`SELECT * FROM users WHERE email='${username}' and password='${password}'`,(err,res)=>{
+                callback(res);
+        })
+    }
+    checkUser(email,callback){
+        this.connection.query(`SELECT * FROM users WHERE 'email='${email}`,(err,res)=>{
+            callback(res)
+        })
+    }
+    signupCoach(){
+        this.connection.query(``,(err,res)=>{
+            callback()
+        })
+    }
+    signupClient(){
+        this.connection.query(``,(err,res)=>{
+            callback()
+        })
     }
 }
--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 import  express from 'express';
-import LogicModel from './logic.js';
+import LogicService from './logic.js';
 import cors from 'cors'
+
+let logMod=new LogicService()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 //TODO: Make cors only apply to routes that need it
 app.use(cors()); 
+app.use(express.json())
 
-app.get('/signup',(req,res)=>{
-    console.log("test2")
+app.post('/signup',(req,res)=>{
+    logMod.signup(req.body,(success)=>{
+        res.send({ok: success})
+    })
     res.status(200).send({
         ok: true
      });
 })
-app.post('/login',(req,res)=>{
-    console.log("test1")
-    res.status(200).send({
-        ok: true
-     });
+app.post('/login',async (req,res)=>{
+    logMod.login(req.body.email,req.body.password,(newRes,message)=>{
+        res.send({
+            ok: newRes,
+            message: message
+        });})
 })
 
 app.listen(PORT,()=>{

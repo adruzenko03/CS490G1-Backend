0d3de53f7ff39b4b974060164d03149e9439f2b1
Ja Xian Chen <jxc4@njit.edu>
2023-12-11 15:04:33 -0500

    changes

--- database.js
+++ database.js
@@ -2,39 +2,54 @@ import mysql from 'mysql2';
 import "dotenv/config.js";
 
 export default class DatabaseService {
-    constructor() {
-        this.connection = mysql.createConnection({
-            multipleStatements: true,
-            host: 'localhost',
-            user: 'root',
-            password: process.env.DB_PASS,
-            database: 'fitness'
-        })
-        this.connection.connect();
-        this.connection.query('SELECT 1 + 1 AS solution', (err, res) => {
-            console.log("Successful Database Connection")
-        })
-    }
-    login(username, password, callback) {
-        this.connection.query(`SELECT * FROM user_auth WHERE email='${username}' and password='${password}'`, (err, res) => {
-            callback(res);
-        })
-    }
-    checkUser(email, callback) {
-        this.connection.query(`SELECT * FROM user_auth WHERE email='${email}'`, (err, res) => {
-            callback(err,res)
-        })
-    }
+  constructor() {
+    this.connection = mysql.createConnection({
+      multipleStatements: true,
+      host: "localhost",
+      user: "root",
+      password: process.env.DB_PASS,
+      database: "fitness",
+    });
+    this.connection.connect();
+    this.connection.query("SELECT 1 + 1 AS solution", (err, res) => {
+      console.log("Successful Database Connection");
+    });
+  }
+  login(username, password, callback) {
+    this.connection.query(
+      `SELECT * FROM user_auth WHERE email='${username}' and password='${password}'`,
+      (err, res) => {
+        if (err) {
+          console.error("Error during login:", err);
+          callback(null, err); 
+          return;
+        }
+        callback(res, null); 
+      }
+    );
+  }
 
-    signupUserAuth(email, password, callback) {
-        this.connection.query(`INSERT INTO user_auth (email, password) VALUES ('${email}','${password}');
-                               SELECT LAST_INSERT_ID() AS userID;`, (err, res) => {
-            callback(err, res)
-        })
+  checkUser(email, callback) {
+    this.connection.query(
+      `SELECT * FROM user_auth WHERE email='${email}'`,
+      (err, res) => {
+        callback(err, res);
+      }
+    );
+  }
 
-    }
-    signupUser(info, userID, callback) {
-        this.connection.query(`INSERT INTO users (user_id, first_name, last_name, phone_number, street_address, city, state, zip, role)
+  signupUserAuth(email, password, callback) {
+    this.connection.query(
+      `INSERT INTO user_auth (email, password) VALUES ('${email}','${password}');
+                               SELECT LAST_INSERT_ID() AS userID;`,
+      (err, res) => {
+        callback(err, res);
+      }
+    );
+  }
+  signupUser(info, userID, callback) {
+    this.connection.query(
+      `INSERT INTO users (user_id, first_name, last_name, phone_number, street_address, city, state, zip, role)
                                VALUES (${userID}, '${info.firstName}', '${info.lastName}', '${info.phoneNumber}', '${info.streetAddress}',
                                        '${info.city}', '${info.state}', '${info.zip}', '${info.role}');
 
@@ -46,27 +61,36 @@ export default class DatabaseService {
                                INSERT INTO users_audit (user_id, first_name, last_name, phone_number, street_address, email, password)
                                SELECT user_id,first_name,last_name,phone_number,street_address, '${info.email}', '${info.password}'
                                FROM users
-                               WHERE user_id=${userID}`, (err, res) => {
-            callback(err)
-        })
-    }
-    deleteUser(user_id, callback) {
-        this.connection.query(`DELETE FROM user_auth WHERE user_id=${user_id}`, (err, res) => {
-            callback(err, res)
-        })
-    }
-    signupCoach(info,userID,callback) {//add goal after you make it singular
-        this.connection.query(`INSERT INTO coach_survey 
+                               WHERE user_id=${userID}`,
+      (err, res) => {
+        callback(err);
+      }
+    );
+  }
+  deleteUser(user_id, callback) {
+    this.connection.query(
+      `DELETE FROM user_auth WHERE user_id=${user_id}`,
+      (err, res) => {
+        callback(err, res);
+      }
+    );
+  }
+  signupCoach(info, userID, callback) {
+    //add goal after you make it singular
+    this.connection.query(
+      `INSERT INTO coach_survey 
         SELECT ${userID},${info.experience},'${info.city}', '${info.state}', ${info.cost}, CURRENT_TIMESTAMP
         FROM goals
-        WHERE goal='${info.fitnessGoal}'`, (err, res) => {
-            callback(err)
-        })
-    }
+        WHERE goal='${info.fitnessGoal}'`,
+      (err, res) => {
+        callback(err);
+      }
+    );
+  }
 
-    getGoals(callback){
-        this.connection.query(`SELECT * FROM goals`, (err,res)=>{
-            callback(res)
-        })
-    }
+  getGoals(callback) {
+    this.connection.query(`SELECT * FROM goals`, (err, res) => {
+      callback(res);
+    });
+  }
 }
--- server.js
+++ server.js
@@ -1,120 +1,42 @@
-import  express from 'express';
-import LogicService from './logic.js';
-import cors from 'cors'
-let logMod=new LogicService()
+import express from "express";
+import LogicService from "./logic.js";
+import cors from "cors";
 
+let logMod = new LogicService();
 const app = express();
 const PORT = process.env.PORT || 3001;
 //TODO: Make cors only apply to routes that need it
+app.use(cors());
+app.use(express.json());
 
-app.use(cors()); 
-app.use(express.json())
+app.post("/signup", (req, res) => {
+  logMod.signup(req.body, (status, resp) => {
+    if (status) {
+      res.status(201).send(resp);
+    } else {
+      res.status(406).send();
+    }
+  });
+  // res.status(200).send({
+  //     ok: true
+  //  });
+});
+app.post("/login", (req, res) => {
+  logMod.login(req.body.email, req.body.password, (status, resp) => {
+    if (status) {
+      res.status(200).send(resp);
+    } else {
+      res.status(404).send(resp);
+    }
+  });
+});
 
-app.post('/signup',(req,res)=>{
-    logMod.signup(req.body,(status,resp)=>{
-        if(status){
-            res.status(201).send(resp);
-        }
-        else{
-            res.status(406).send();
-        }
-    })
-    // res.status(200).send({
-    //     ok: true
-    //  });
-})
-app.post('/login',(req,res)=>{
-    logMod.login(req.body.email,req.body.password,(status,resp)=>{
-        if(status){
-            res.status(200).send(resp);
-        }
-        else{
-            res.status(404).send(resp);
-        }
-    })
-})
+app.get("/goals", (req, res) => {
+  logMod.getGoals((resp) => {
+    res.status(200).send(resp);
+  });
+});
 
-app.get('/goals',(req,res)=>{
-    logMod.getGoals((resp)=>{
-        res.status(200).send(resp)
-    })
-})
-
-app.get('/clientRequestsFetch/:coachId', async(req, res)=>{
-    const {coachId} = req.params;
-    console.log("Made to Server.js", coachId);
-    dataMod.getPendingClientRequests(coachId, (err, surveyData)=>{
-        if(err){
-            res.status(500).json({ok:false, error:err.message});
-        }else{
-            res.status(200).json({ok:true, surveyData});
-        }
-    })
-})
-
-app.get('/acceptedClients/:coachId', async(req, res)=>{
-    const {coachId} = req.params;
-    console.log("Made to Server.js", coachId);
-    dataMod.getAcceptedClients(coachId, (err, surveyData)=>{
-        if(err){
-            res.status(500).json({ok:false, error:err.message});
-        }else{
-            res.status(200).json({ok:true, surveyData});
-        }
-    })
-})
-
-app.get('/goalsList', async(req, res)=>{
-    console.log("Made to Server.js");
-    dataMod.getGoalsList((err, surveyData)=>{
-        if(err){
-            res.status(500).json({ok:false, error:err.message});
-        }else{
-            res.status(200).json({ok:true, surveyData});
-        }
-    })
-})
-app.get('/experienceList', async(req, res)=>{
-    console.log("Made to Server.js");
-    dataMod.getExperienceList((err, surveyData)=>{
-        if(err){
-            res.status(500).json({ok:false, error:err.message});
-        }else{
-            res.status(200).json({ok:true, surveyData});
-        }
-    })
-})
-app.get('/locationList', async(req, res)=>{
-    console.log("Made to Server.js");
-    dataMod.getLocationList((err, surveyData)=>{
-        if(err){
-            res.status(500).json({ok:false, error:err.message});
-        }else{
-            res.status(200).json({ok:true, surveyData});
-        }
-    })
-})
-app.get('/costList', async(req, res)=>{
-    console.log("Made to Server.js");
-    dataMod.getPriceList((err, surveyData)=>{
-        if(err){
-            res.status(500).json({ok:false, error:err.message});
-        }else{
-            res.status(200).json({ok:true, surveyData});
-        }
-    })
-})
-app.get('/coachList', async(req, res)=>{
-    console.log("Made to Server.js");
-    dataMod.getCoachList((err, surveyData)=>{
-        if(err){
-            res.status(500).json({ok:false, error:err.message});
-        }else{
-            res.status(200).json({ok:true, surveyData});
-        }
-    })
-})
-
-app.listen(PORT,()=>{
-    console.log("Listening on port "+ PORT)
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
 });

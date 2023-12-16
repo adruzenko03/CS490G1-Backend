b7ea9d48734f8cb79ebe8faf05bd44da3ac1c5c9
Jordyn Lacayanga <jordynlacayanga@Jordyns-MBP.home>
2023-12-16 10:41:25 -0500

    merging dev

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import LogicService from "./logic.js";
 import cors from "cors";
+import DatabaseService from "./database.js";
 
 let logMod = new LogicService();
-
+let dataMod= new DatabaseService()
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
@@ -31,12 +32,6 @@ app.post("/login", (req, res) => {
   });
 });
 
-app.get("/goals", (req, res) => {
-  logMod.getGoals((resp) => {
-    res.status(200).send(resp);
-  });
-});
-/*
 app.post("/coach-survey", async (req, res) => {
   const surveyData = req.body;
   console.log("Coach Survey Data:", surveyData);
@@ -85,18 +80,6 @@ app.get("/acceptedClients/:userId", async (req, res) => {
   });
 });
 
-app.get("/clientRequestsFetch/:userId", async (req, res) => {
-  const { userId } = req.params;
-  console.log("Made to Server.js via clientRequestsFetch", userId);
-  dataMod.getSurveyData(userId, (err, surveyData) => {
-    if (err) {
-      res.status(500).json({ ok: false, error: err.message });
-    } else {
-      res.status(200).json({ ok: true, surveyData });
-    }
-  });
-});
-*/
 app.get("/exercises", (req, res) => {
   logMod.getExercises((success, result) => {
     if (success) {
@@ -108,6 +91,55 @@ app.get("/exercises", (req, res) => {
     }
   });
 
+}); 
+
+
+app.get('/clientRequestsFetch/:userId', async (req, res) => {
+    const {userId} = req.params;
+    console.log("Made to Server.js via clientRequestsFetch", userId);
+    logMod.getClientRequests(userId, (err, Data) => {
+        if(err){
+            res.status(500).json({ok:false, error: err.message});
+        } else {
+            res.status(200).json({ok:true, Data});
+        }
+    });
+});
+
+//ALL CLIENTS BELOW ARE NOT DOCUMENTED
+app.delete('/removeClient/:userId', (req, res) => {
+    const { userId } = req.params;
+    logMod.removeClient(userId, (err, result) => {
+        if (err) {
+            res.status(500).send('Error removing client');
+        } else {
+            res.status(200).send('Client removed successfully');
+        }
+    });
+});
+
+app.post('/acceptClient/:userId', (req, res) => {
+    const {userId} = req.params;
+    console.log("Client id", userId);
+    logMod.acceptClient(userId, (err, result) => {
+        if (err) {
+            res.status(500).send('Error accepting client');
+        } else {
+            res.status(200).send('Client accepted successfully');
+        }
+    });
+});
+
+app.post('/declineClient/:userId', (req, res) => {
+    const {userId} = req.params;
+    console.log("Client id", userId);
+    logMod.declineClient(userId, (err, result) => {
+        if (err) {
+            res.status(500).send('Error declining client');
+        } else {
+            res.status(200).send('Client declined successfully');
+        }
+    });
 });
 
 app.get("/workouts", (req, res) => {
@@ -120,6 +152,74 @@ app.get("/workouts", (req, res) => {
         .json({ ok: false, message: "Error retrieving workouts" });
     }
   });
+});
+
+app.get("/myworkouts/:userId", (req, res) => {
+  const userId = req.params.userId;
+  logMod.getUserWorkouts(userId, (success, result) => {
+    if (success) {
+      res.status(200).json({ ok: true, exercises: result });
+    } else {
+      res.status(500).json({ ok: false, message: "Error retrieving workouts" });
+    }
+  });
+});
+
+
+app.delete("/workoutsremoved", (req, res) => {
+  const { userId, workoutId } = req.body;
+
+  logMod.deleteUserWorkout(userId, workoutId, (success, message, insertId) => {
+    if (success) {
+      res.status(200).json({ ok: true, message });
+    } else {
+      res.status(500).json({ ok: false, message });
+    }
+  });
+});
+
+app.post("/workoutsadded", (req, res) => {
+  const { userId, workoutId } = req.body;
+  logMod.insertUserWorkout(userId, workoutId, (success, message, insertId) => {
+    if (success) {
+      res.status(201).json({ ok: true, message, insertId });
+    } else {
+      res.status(500).json({ ok: false, message });
+    }
+  });
+});
+
+app.get("/activities/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  logMod.getActivity(userId,(success, activities) => {
+    if (success) {
+      res.status(200).json({ ok: true, activities });
+    } else {
+      res
+        .status(500)
+        .json({ ok: false, message: "Error retrieving activities" });
+    }
+  });
+});
+ 
+
+app.post("/activitySurvey", (req, res) => {
+  const { userId, entryDate, calorieIntake, bodyWeight } = req.body;
+
+  logMod.insertUserDailyActivity(
+    userId,
+    entryDate,
+    calorieIntake,
+    bodyWeight,
+    (success, message, insertId) => {
+      if (success) {
+        res.status(201).json({ ok: true, message, insertId });
+      } else {
+        res.status(500).json({ ok: false, message });
+      }
+    }
+  );
 });
 
 app.get('/pendingCoaches', (req, res) => {
@@ -186,4 +286,4 @@ app.post('/updateExercise/:exercise_id/:actionType', (req, res) => {
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
-});
+})
